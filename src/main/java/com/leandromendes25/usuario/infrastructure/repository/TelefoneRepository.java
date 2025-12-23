f52a55145860a25a08b5752f9ead91ceb83e20fa
leandromendes25 <leandromiendes@hotmail.com>
2025-12-23 15:30:40 -0300

    correcao importacao das entites

--- src/main/java/com/leandromendes25/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/leandromendes25/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,6 @@
 package com.leandromendes25.usuario.infrastructure.repository;
 
-import com.example.praticando.spring.infrascturue.entity.Telefone;
+import com.leandromendes25.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
