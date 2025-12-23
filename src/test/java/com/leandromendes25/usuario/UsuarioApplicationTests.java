43f03afebaf41d2dcc08a2a10e6e93268ed2141d
leandromendes25 <leandromiendes@hotmail.com>
2025-12-23 10:46:38 -0300

    commit inicial

---
+++ src/test/java/com/leandromendes25/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandromendes25.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
